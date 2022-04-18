d9ee2c50c6df1200e593c6d7c3009ee4504ccfb4
MrMreal <mohamadreza.alvanian@gmail.com>
2022-04-19 02:22:16 +0430

    master

--- pages/index.js
+++ pages/index.js
@@ -53,30 +53,18 @@ export default function Home() {
           </div>
         </div>
         <div className={sidecss}>
-          <aside className="w-64 bg-white" aria-label="sidenavExample">
-            <div class="pt-4 pb-2 px-6">
-              <a href="#!">
-                <div class="flex items-center">
-                  <div class="shrink-0"></div>
-                  <div class="grow ml-3">
-                    <p class="text-sm font-semibold text-blue-600">
-                      Jason McCoel
-                    </p>
-                  </div>
-                </div>
-              </a>
-            </div>
-            <ul class="relative px-1">
-              <li class="relative">
+          <aside className="w-full bg-white" aria-label="sidenavExample">
+            <ul className="relative px-1">
+              <li className="relative">
                 <a
-                  class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded"
-                  href="#!"
+                  className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden hover:text-blue-600 hover:bg-blue-50 text-gray-700 text-ellipsis whitespace-nowrap rounded"
+                  href="#"
                 >
                   <svg
                     aria-hidden="true"
                     focusable="false"
                     data-prefix="fas"
-                    class="w-3 h-3 mr-3"
+                    className="w-5 h-5 ml-3"
                     role="img"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 512 512"
@@ -86,14 +74,12 @@ export default function Home() {
                       d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"
                     ></path>
                   </svg>
-                  <span>Non-collapsible link</span>
+                  <span>صفحه اصلی</span>
                 </a>
               </li>
               <li class="relative" id="sidenavSecEx2">
                 <a
-                  class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-blue-600 hover:bg-blue-50 transition duration-300 ease-in-out cursor-pointer"
-                  data-mdb-ripple="true"
-                  data-mdb-ripple-color="primary"
+                  class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden  hover:text-blue-600 hover:bg-blue-50 text-gray-700 text-ellipsis whitespace-nowrap transition duration-300 ease-in-out cursor-pointer"
                   data-bs-toggle="collapse"
                   data-bs-target="#collapseSidenavSecEx2"
                   aria-expanded="false"
@@ -103,7 +89,7 @@ export default function Home() {
                     aria-hidden="true"
                     focusable="false"
                     data-prefix="fas"
-                    class="w-3 h-3 mr-3"
+                    class="w-5 h-5 ml-3"
                     role="img"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 496 512"
@@ -113,12 +99,12 @@ export default function Home() {
                       d="M336.5 160C322 70.7 287.8 8 248 8s-74 62.7-88.5 152h177zM152 256c0 22.2 1.2 43.5 3.3 64h185.3c2.1-20.5 3.3-41.8 3.3-64s-1.2-43.5-3.3-64H155.3c-2.1 20.5-3.3 41.8-3.3 64zm324.7-96c-28.6-67.9-86.5-120.4-158-141.6 24.4 33.8 41.2 84.7 50 141.6h108zM177.2 18.4C105.8 39.6 47.8 92.1 19.3 160h108c8.7-56.9 25.5-107.8 49.9-141.6zM487.4 192H372.7c2.1 21 3.3 42.5 3.3 64s-1.2 43-3.3 64h114.6c5.5-20.5 8.6-41.8 8.6-64s-3.1-43.5-8.5-64zM120 256c0-21.5 1.2-43 3.3-64H8.6C3.2 212.5 0 233.8 0 256s3.2 43.5 8.6 64h114.6c-2-21-3.2-42.5-3.2-64zm39.5 96c14.5 89.3 48.7 152 88.5 152s74-62.7 88.5-152h-177zm159.3 141.6c71.4-21.2 129.4-73.7 158-141.6h-108c-8.8 56.9-25.6 107.8-50 141.6zM19.3 352c28.6 67.9 86.5 120.4 158 141.6-24.4-33.8-41.2-84.7-50-141.6h-108z"
                     ></path>
                   </svg>
-                  <span>Collapsible item 1</span>
+                  <span>مدیریت محتوا</span>
                   <svg
                     aria-hidden="true"
                     focusable="false"
                     data-prefix="fas"
-                    class="w-3 h-3 ml-auto"
+                    class="w-3 h-3 mr-auto"
                     role="img"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 448 512"
@@ -137,8 +123,8 @@ export default function Home() {
                 >
                   <li class="relative">
                     <a
-                      href="#!"
-                      class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-blue-600 hover:bg-blue-50 transition duration-300 ease-in-out"
+                      href="#"
+                      class="flex items-center text-xs py-4 pr-12 pl-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap hover:text-blue-600 hover:bg-blue-50 transition duration-300 ease-in-out"
                       data-mdb-ripple="true"
                       data-mdb-ripple-color="primary"
                     >
@@ -148,7 +134,7 @@ export default function Home() {
                   <li class="relative">
                     <a
                       href="#!"
-                      class="flex items-center text-xs py-4 pl-12 pr-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-blue-600 hover:bg-blue-50 transition duration-300 ease-in-out"
+                      class="flex items-center text-xs py-4 pr-12 pl-6 h-6 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap hover:text-blue-600 hover:bg-blue-50 transition duration-300 ease-in-out"
                       data-mdb-ripple="true"
                       data-mdb-ripple-color="primary"
                     >
