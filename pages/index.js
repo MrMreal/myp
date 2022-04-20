2c31338af65f6f87b652999ed9c38caa8afb756a
MrMreal <mohamadreza.alvanian@gmail.com>
2022-04-20 17:05:00 +0430

    master

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,16 @@ import { HiOutlineCog } from "react-icons/hi";
 import { HiOutlineHome } from "react-icons/hi";
 import { HiOutlineDocumentText } from "react-icons/hi";
 import { HiOutlineChevronDown } from "react-icons/hi";
+import { HiOutlineUser } from "react-icons/hi";
 
 export default function Home() {
   const [menu, setMenu] = useState(true);
   const sidecss = menu
-    ? "w-64 fixed right-0 overflow-y-scroll bottom-0 top-16 bg-white shadow mr-0 transition-all duration-1000"
-    : "w-64 fixed right-0 overflow-y-scroll bottom-0 top-16 bg-white shadow -mr-40 transition-all duration-1000 translate-x-40";
+    ? "w-48 fixed right-0 overflow-y-scroll bottom-0 top-16 bg-white shadow mr-0 transition-all duration-1000"
+    : "w-48 fixed right-0 overflow-y-scroll bottom-0 top-16 bg-white shadow -mr-40 transition-all duration-1000 translate-x-40";
 
   const maincss = menu
-    ? "flex flex-1 flex-col mt-16 p-4 mr-64 bg-gray-100 space-y-4 transition-all duration-1000"
+    ? "flex flex-1 flex-col mt-16 p-4 mr-48 bg-gray-100 space-y-4 transition-all duration-1000"
     : "flex flex-1 flex-col mt-16 p-4 mr-0 bg-gray-100 space-y-4 transition-all duration-1000";
 
   return (
@@ -32,7 +33,9 @@ export default function Home() {
                 }}
               />
             </div>
-            <div className="flex font-semibold text-3xl ">Alvanian</div>
+            <div className="hidden sm:flex font-semibold text-3xl ">
+              Alvanian
+            </div>
             <div className="flex gap-2">
               <HiOutlineBell
                 className="w-7 h-7 hover:cursor-pointer hover:text-red-500"
@@ -46,12 +49,104 @@ export default function Home() {
                   setMenu(!menu);
                 }}
               />
-              <HiOutlineMenu
-                className="w-7 h-7 hover:cursor-pointer hover:text-red-500"
-                onClick={() => {
-                  setMenu(!menu);
-                }}
-              />
+
+              <div class="dropdown relative">
+                <HiOutlineUser
+                  className="w-7 h-7 hover:cursor-pointer hover:text-red-500 "
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                  id="dropdownMenuButton1"
+                />
+                <ul
+                  class="
+          dropdown-menu
+          min-w-max
+          absolute
+          
+          bg-white
+          text-base
+          z-50
+          float-left
+          py-2
+          list-none
+          text-left
+          rounded-lg
+          shadow-lg
+          mt-1
+          hidden
+          m-0
+          bg-clip-padding
+          border-none"
+                  aria-labelledby="dropdownMenuButton1"
+                >
+                  <li>
+                    <a
+                      class="
+              dropdown-item
+              text-sm
+              py-2
+              px-4
+              font-normal
+              block
+              w-full
+              whitespace-nowrap
+              bg-transparent
+              text-gray-700
+              hover:bg-gray-100
+            "
+                      href="#"
+                    >
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      class="
+              dropdown-item
+              text-sm
+              py-2
+              px-4
+              font-normal
+              block
+              w-full
+              whitespace-nowrap
+              bg-transparent
+              text-gray-700
+              hover:bg-gray-100
+            "
+                      href="#"
+                    >
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      class="
+              dropdown-item
+              text-sm
+              py-2
+              px-4
+              font-normal
+              block
+              w-full
+              whitespace-nowrap
+              bg-transparent
+              text-gray-700
+              hover:bg-gray-100
+            "
+                      href="#"
+                    >
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              {/* <Image
+                className="rounded-full w-7 border hover:cursor-pointer"
+                width={28}
+                height={28}
+                src="/1.webp"
+              /> */}
             </div>
           </div>
         </div>
